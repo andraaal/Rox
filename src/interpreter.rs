197c0d9c01e361edc7187db150d9af9a32ad6aad
andi-coding <a.muth@gmx.at>
2025-11-28 21:23:37 +0100

    Fixed interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -19,7 +19,7 @@ impl Interpreter {
             Expr::Bool(b) => Value::Bool(b),
             Expr::Number(f) => Value::Number(f),
             Expr::String(s) => Value::String(s),
-            Expr::Negate(e) => self.expression(*e),
+            Expr::Negate(e) => self.unary_negate(*e),
             Expr::Add(a, b) => self.numeric_op(*a, *b, |a, b| Value::Number(a + b)),
             Expr::Sub(a, b) => self.numeric_op(*a, *b, |a, b| Value::Number(a - b)),
             Expr::Mul(a, b) => self.numeric_op(*a, *b, |a, b| Value::Number(a * b)),
@@ -34,15 +34,23 @@ impl Interpreter {
         }
     }
 
-    fn numeric_op(&mut self, right: Expr, left: Expr, func: fn(f64, f64) -> Value) -> Value {
-        let a = self.expression(right);
-        let b = self.expression(left);
+    fn unary_negate(&mut self, expr: Expr) -> Value {
+        if let Value::Number(n) = self.expression(expr) {
+            Value::Number(-n)
+        } else {
+            self.runtime_error("Type mismatch: unary '-' applied to non-number");
+        }
+    }
+
+    fn numeric_op(&mut self, left: Expr, right: Expr, func: fn(f64, f64) -> Value) -> Value {
+        let a = self.expression(left);
+        let b = self.expression(right);
 
         if let (Value::Number(a), Value::Number(b)) = (a, b) {
             return
                 func(a, b);
         }
-        self.runtime_error("Type mismatch at ??? - Error locations not yet implemented");
+        self.runtime_error("Type mismatch in numeric operation at ??? - Error locations not yet implemented");
     }
 
     fn comparison(&mut self, left: Expr, right: Expr, func: fn(Value, Value) -> bool) -> Value {
